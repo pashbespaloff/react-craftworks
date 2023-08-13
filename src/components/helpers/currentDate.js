62cd4cddd44c860d0a2dfa54af37e678a09b7590
pash bespaloff <123310036+pashbespaloff@users.noreply.github.com>
2023-08-14 00:06:42 +0200

    minor clock fix

--- src/components/helpers/currentDate.js
+++ src/components/helpers/currentDate.js
@@ -1,4 +1,6 @@
 const current = new Date();
-const optionalZero = current.getHours() < 10 ? 0 : null;
-export const time = `${optionalZero}${current.getHours()}:${optionalZero}${current.getMinutes()}`;
+const hourZero = current.getHours() < 10 ? 0 : null;
+const minutesZero = current.getMinutes() < 10 ? 0 : null;
+
+export const time = `${hourZero}${current.getHours()}:${minutesZero}${current.getMinutes()}`;
 export const date = `${current.getDate()}.${current.getMonth()+1}.${current.getFullYear()}`;
