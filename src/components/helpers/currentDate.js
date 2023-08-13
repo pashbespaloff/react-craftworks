1a48773d635996c264ab5030412956d88ebb4719
pash bespaloff <123310036+pashbespaloff@users.noreply.github.com>
2023-08-14 00:12:01 +0200

    clock bug fixed

--- src/components/helpers/currentDate.js
+++ src/components/helpers/currentDate.js
@@ -1,6 +1,6 @@
 const current = new Date();
-const hourZero = current.getHours() < 10 ? 0 : null;
-const minutesZero = current.getMinutes() < 10 ? 0 : null;
+const hourZero = current.getHours() < 10 ? 0 : "";
+const minutesZero = current.getMinutes() < 10 ? 0 : "";
 
 export const time = `${hourZero}${current.getHours()}:${minutesZero}${current.getMinutes()}`;
 export const date = `${current.getDate()}.${current.getMonth()+1}.${current.getFullYear()}`;
